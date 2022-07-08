672ac99119551973fca900af3546ca78ed0a405b
Emil Urechean <emil.urechean@gmail.com>
2022-07-08 14:29:45 +0300

    fixed aws_cognito_user_pool

--- iac/aws_cognito_user_pool/locals.tf
+++ iac/aws_cognito_user_pool/locals.tf
@@ -5,5 +5,5 @@ locals {
     key    = var.src.config_key
   }
 
-  email_configuration_source = var.src.email_configuration.name == null ? null : format("arn:aws:ses:%s:%s:identity/%s", local.ftl_active, data.aws_caller_identity.this.account_id, var.src.email_configuration.name)
+  # email_configuration_source = var.src.email_configuration.name == null ? null : format("arn:aws:ses:%s:%s:identity/%s", local.ftl_active, data.aws_caller_identity.this.account_id, var.src.email_configuration.name)
 }
